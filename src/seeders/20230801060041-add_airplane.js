beb4425a92b87172f687b8d8a326da37e01a16dd
abhay2002-pro <abhayray2002@gmail.com>
2023-08-01 22:32:45 +0530

    added seeders

---
+++ src/seeders/20230801060041-add_airplane.js
@@ -0,0 +1,46 @@
+"use strict";
+const { Op } = require("sequelize");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Airplanes",
+      [
+        {
+          modelNumber: "airbus380",
+          capacity: 900,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          modelNumber: "boeing777",
+          capacity: 450,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Airplanes", {
+      [Op.or]: [{ modelNumber: "airbus380" }, { modelNumber: "boeing777" }],
+    });
+  },
+};
