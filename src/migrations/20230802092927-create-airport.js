31f5ece99190e111d74b676d8ceb76ddc5f7de8f
abhay2002-pro <abhayray2002@gmail.com>
2023-08-02 18:17:53 +0530

    Added airports model and migration

---
+++ src/migrations/20230802092927-create-airport.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      address: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        unique: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
+};
